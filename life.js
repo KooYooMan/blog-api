c58f6b70831b3ec085449fd8fc5d44f3bfa9b15f
kooyooman <kooyooman@gmail.com>
2020-11-22 00:16:29 +0700

    blog-api initialization

---
+++ life.js
@@ -0,0 +1,107 @@
+const birthday = new Date("2000-12-19")
+const end_date = new Date("2070-12-19")
+const birthday_color = '#F89542'
+const default_color = '#1AA9FF'
+
+let events = [];
+
+const event_end_date = (e) => {
+  if (e.date_end) return new Date(e.date_end)
+  else return new Date(e.date_start);
+}
+
+events = events.sort((e1, e2) => {
+  let e1ref = event_end_date(e1);
+  let e2ref = event_end_date(e2);
+  if (e2ref > e1ref) return 1;
+  else if (e2ref < e1ref) return -1;
+  else return 0;
+})
+
+const get_end = () => {
+  return end_date
+}
+
+const date_to_string = (date) => {
+  var d = date.getDate();
+  var month = date.getMonth() + 1;
+  var day = d < 10 ? '0' + d : '' + d;
+  if (month < 10) month = '0' + month;
+  return day + '/' + month + '/' + date.getFullYear()
+}
+
+const get_events_in_week = (week_start, week_end) => {
+  const date = date_to_string(week_start)
+  let color = default_color;
+  let _events = events.filter((e) => {
+    let estart = new Date(e.date_start);
+    let eend = event_end_date(e)
+    // let eend = new Date(e.date_end);
+    let start_in_week = estart >= week_start && estart < week_end;
+    let end_in_week = eend >= week_start && eend < week_end;
+    let event_spans_week = estart <= week_start && eend >= week_end;
+    let in_week = start_in_week || end_in_week || event_spans_week;
+    if (in_week) {
+      if (e.color) color = e.color;
+    }
+    return in_week;
+  });
+
+  _events = _events.map(value => value.title)
+
+  if (birthday) {
+    let age = 0;
+    let bd_in_week = false;
+    while (week_start < week_end) {
+      if (week_start.getMonth() == birthday.getMonth() && week_start.getDate() == birthday.getDate()) {
+        bd_in_week = true;
+        age = week_start.getFullYear() - birthday.getFullYear();
+        break;
+      }
+      week_start.setDate(week_start.getDate() + 1);
+    }
+    if (bd_in_week) {
+      color = birthday_color;
+      let title;
+      if (age == 0) {
+        title = `I was born!`;
+      } else {
+        title = `I turn ${age} on ${birthday.getDate()}/${birthday.getMonth() + 1}`;
+      }
+      _events.push(title);
+    }
+  }
+  const tooltip = [date].concat(_events).join(', ')
+  return {
+    'tooltip': tooltip,
+    'color': color,
+  };
+}
+
+const all_weeks = (fn) => {
+  let end = get_end();
+  let cursor = new Date(birthday.getTime());
+  while (cursor <= end) {
+    let d = new Date(cursor.getTime());
+    cursor.setDate(cursor.getDate() + 7);
+    fn(d, new Date(cursor.getTime()));
+  }
+}
+
+const render_all_weeks = (list_event) => {
+  events = list_event.map(event => ({
+    date_start: new Date(event.date_start),
+    date_end: new Date(event.date_end),
+    title: event.title,
+    color: event.color
+  }));
+  let weeks = [];
+  all_weeks((start, end) => {
+    weeks.push(get_events_in_week(start, end));
+  });
+  return weeks;
+}
+
+module.exports = {
+  render_all_weeks
+};
---
+++ server.js
@@ -0,0 +1,84 @@
+// server.js
+
+const express = require("express");
+const life = require("./life");
+const server = express();
+
+const body_parser = require("body-parser");
+
+// parse JSON (application/json content-type)
+server.use(body_parser.json());
+
+const port = process.env.PORT || 3000;
+
+// << db setup >>
+const db = require("./db");
+const dbName = "Blog";
+const collectionName = "Life";
+
+// << db init >>
+
+db.initialize(dbName, collectionName, function (dbCollection) { // successCallback
+  // get all items
+  // dbCollection.find().toArray(function (err, result) {
+  //   if (err) throw err;
+  //   console.log(result);
+  // });
+
+  // << db CRUD routes >>
+  server.get("/events", (request, response) => {
+    // return updated list
+    dbCollection.find().toArray((error, result) => {
+      if (error) throw error;
+      response.json(life.render_all_weeks(result));
+    });
+  });
+
+  server.post("/event", (request, response) => {
+    const event = request.body;
+    dbCollection.insertOne(event, (error, result) => { // callback of insertOne
+      if (error) throw error;
+      // return updated list
+      dbCollection.find().toArray((_error, _result) => { // callback of find
+        if (_error) throw _error;
+        response.json(_result);
+      });
+    });
+  });
+
+  server.put("/items/:id", (request, response) => {
+    const itemId = request.params.id;
+    const item = request.body;
+    console.log("Editing item: ", itemId, " to be ", item);
+
+    dbCollection.updateOne({ id: itemId }, { $set: item }, (error, result) => {
+      if (error) throw error;
+      // send back entire updated list, to make sure frontend data is up-to-date
+      dbCollection.find().toArray(function (_error, _result) {
+        if (_error) throw _error;
+        response.json(_result);
+      });
+    });
+  });
+
+  server.delete("/items/:id", (request, response) => {
+    const itemId = request.params.id;
+    console.log("Delete item with id: ", itemId);
+
+    dbCollection.deleteOne({ id: itemId }, function (error, result) {
+      if (error) throw error;
+      // send back entire updated list after successful request
+      dbCollection.find().toArray(function (_error, _result) {
+        if (_error) throw _error;
+        response.json(_result);
+      });
+    });
+  });
+
+}, function (err) { // failureCallback
+  throw (err);
+});
+
+server.listen(port, () => {
+  console.log(`Server listening at ${port}`);
+});
